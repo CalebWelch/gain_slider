60a07386022c26d3bd87d5802314124cf0e8a670
Welch <CW056762@cerner.net>
2018-02-21 10:22:06 -0600

    init commit

---
+++ Source/PluginEditor.h
@@ -0,0 +1,36 @@
+/*
+  ==============================================================================
+
+    This file was auto-generated!
+
+    It contains the basic framework code for a JUCE plugin editor.
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#include "../JuceLibraryCode/JuceHeader.h"
+#include "PluginProcessor.h"
+
+
+//==============================================================================
+/**
+*/
+class Delay_not_dumbAudioProcessorEditor  : public AudioProcessorEditor
+{
+public:
+    Delay_not_dumbAudioProcessorEditor (Delay_not_dumbAudioProcessor&);
+    ~Delay_not_dumbAudioProcessorEditor();
+
+    //==============================================================================
+    void paint (Graphics&) override;
+    void resized() override;
+	Slider gainSlider;
+private:
+    // This reference is provided as a quick way for your editor to
+    // access the processor object that created it.
+    Delay_not_dumbAudioProcessor& processor;
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (Delay_not_dumbAudioProcessorEditor)
+};
