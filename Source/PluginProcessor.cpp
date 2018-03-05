2beb15972cdab0441ce8476ebc1030983d79623e
Welch <CW056762@cerner.net>
2018-03-05 15:42:26 -0600

    fixed some stuff

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -142,13 +142,7 @@ void Delay_not_dumbAudioProcessor::processBlock (AudioBuffer<float>& buffer, Mid
     auto totalNumInputChannels  = getTotalNumInputChannels();
     auto totalNumOutputChannels = getTotalNumOutputChannels();
 	float current_gain = pow(10, *state_tree.getRawParameterValue(GAIN_ID) / 20);
-	if (current_gain == previous_gain){
-		buffer.applyGain(current_gain);
-	}
-	else {
-		buffer.applyGainRamp(0, buffer.getNumSamples(), previous_gain, current_gain);
-		previous_gain = current_gain;
-	}
+	
     // in case we have more outputs than inputs, this code clears any output
     // channels that didn't contain input data, (because these aren't
     // guaranteed to be empty - they may contain garbage).
